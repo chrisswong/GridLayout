24716295d929a1f1a12f3297dcc825777f2c41d3
Chris Wong <chriswong.broadband@gmail.com>
2015-06-13 09:14:56 +0800

    Changed the collectionView delegate to ViewController. Changed the gridLayout delegate to ViewController. Move the UICollectionViewLayout Methods to ViewController.

--- GridLayout/ViewController.m
+++ GridLayout/ViewController.m
@@ -38,4 +38,34 @@
   return 100;
 }
 
+#pragma mark - UICollectionViewDelegateFlowLayout
+////////////////////////////////////////////////////////////////////////////
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+      GridLayout *layout = (GridLayout *)self.collectionViewLayout;
+    CGFloat width = 0.0;
+    if (layout.column > 1) {
+        width = (collectionView.frame.size.width-layout.padding*(layout.column-1))/layout.column;
+    } else {
+        width = collectionView.frame.size.width;
+    }
+    return CGSizeMake(floorf(width), width*layout.aspectRatio);
+}
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsZero;
+}
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    GridLayout *layout = (GridLayout *)self.collectionViewLayout;
+    return layout.padding;
+}
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    GridLayout *layout = (GridLayout *)self.collectionViewLayout;
+    return layout.padding;
+}
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section {
+    return CGSizeZero;
+}
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section {
+    return CGSizeZero;
+}
+
 @end
